72c543df16a61ea650d26378624e88c568e7cce8
snifjcy <73164655+snifjcy@users.noreply.github.com>
2021-10-05 00:42:34 +0200

    added option to close sidebar by clicking on the title

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ toggleSidebar = () => {
 }
 document.getElementById("openButton").addEventListener("click", toggleSidebar)
 document.getElementById("closeButton").addEventListener("click", toggleSidebar)
+
+document.getElementById("title").addEventListener("click", () => {
+    if (document.getElementById("sidebar").classList.contains("open"))
+        toggleSidebar()
+})
