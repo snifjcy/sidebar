7cae44a0d72beb9df2da4228bc6623a573937c3a
Halí V <halivelazquez@outlook.com>
2021-10-04 20:13:30 -0500

    Add support for close sidebar on document click

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-toggleSidebar = () => {
+toggleSidebar = (evt) => {
+    evt?.stopPropagation();
     document.getElementById("sidebar").classList.toggle("open")
 }
 document.getElementById("openButton").addEventListener("click", toggleSidebar)
 document.getElementById("closeButton").addEventListener("click", toggleSidebar)
 
-document.getElementById("title").addEventListener("click", () => {
+document.getElementById("sidebar").addEventListener("click", (evt) => {
+    evt.stopPropagation()
+})
+
+document.addEventListener("click", (event) => {
     if (document.getElementById("sidebar").classList.contains("open"))
         toggleSidebar()
 })
